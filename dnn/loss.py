88091208384dde5bfa069894337723c35c59219b
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-06-14 22:07:52 +0530

    Add ability to use loss_factory with user-defined loss functions
    Also move loss_factory to utils

--- dnn/loss.py
+++ dnn/loss.py
@@ -1,14 +1,34 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
-from abc import abstractmethod
 
 
-class Loss:
-    def __init__(self, Y):
+class Loss(ABC):
+    name = None
+
+    def __init__(self, Y, *args, **kwargs):
         self.labels = Y
         self.train_size = Y.shape[-1]
 
+    @staticmethod
+    def _add_sub_cls(sub_cls):
+        name = sub_cls.name
+        if isinstance(name, (list, tuple)):
+            return {n: sub_cls for n in name}
+        return {name: sub_cls}
+
+    @classmethod
+    def _get_loss_classes(cls):
+        result = {}
+
+        for sub_cls in cls.__subclasses__():
+            result.update(sub_cls._get_loss_classes())
+            if sub_cls.name is not None:
+                result.update(cls._add_sub_cls(sub_cls))
+        return result
+
     def validate_input(self, preds):
-        if self.Y.shape != self.preds.shape:
+        if self.labels.shape != preds.shape:
             raise AttributeError(
                 "The labels and the predictions should have the same shape"
             )
@@ -22,13 +42,17 @@ class Loss:
         pass
 
     def compute_loss(self, preds):
+        self.validate_input(preds)
         return self.loss_func(preds)
 
     def compute_derivatives(self, preds):
+        self.validate_input(preds)
         return self.loss_derivative(preds)
 
 
 class BinaryCrossEntropy(Loss):
+    name = ["binary_crossentropy", "bse"]
+
     def loss_func(self, preds):
         positive_labels = self.labels * np.log(preds)
         negative_labels = (1 - self.labels) * np.log(1 - preds)
@@ -42,18 +66,11 @@ class BinaryCrossEntropy(Loss):
 
 
 class MeanSquaredError(Loss):
+    name = ["mean_squared_error", "mse"]
+
     def loss_func(self, preds):
         loss = np.sum((preds - self.labels) ** 2) / (2 * self.train_size)
         return np.squeeze(loss)
 
     def loss_derivative(self, preds):
         return (preds - self.labels) / self.train_size
-
-
-def loss_factory(loss, Y):
-    return {
-        "binary_crossentropy": BinaryCrossEntropy(Y),
-        "bse": BinaryCrossEntropy(Y),
-        "mean_squared_error": MeanSquaredError(Y),
-        "mse": MeanSquaredError(Y)
-    }[loss]
--- dnn/utils.py
+++ dnn/utils.py
@@ -1,4 +1,5 @@
 from .activations import Activation
+from .loss import Loss
 
 
 def activation_factory(activation, ip=None, *args, **kwargs):
@@ -7,3 +8,11 @@ def activation_factory(activation, ip=None, *args, **kwargs):
     if cls is None:
         raise ValueError("Activation with this name does not exist")
     return cls(ip=ip, *args, **kwargs)
+
+
+def loss_factory(loss, Y, *args, **kwargs):
+    registry = Loss._get_activation_classes()
+    cls = registry.get(loss)
+    if cls is None:
+        raise ValueError("Loss with this name does not exist")
+    return cls(Y, *args, **kwargs)

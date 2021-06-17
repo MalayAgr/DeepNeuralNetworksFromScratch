22c74cfdd7985a687609fdc65d19f41769d328ed
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-06-18 03:19:07 +0530

    Add function to generate batches of data

--- dnn/utils.py
+++ dnn/utils.py
@@ -1,5 +1,6 @@
 from .activations import Activation
 from .loss import Loss
+import numpy as np
 
 
 def activation_factory(activation, *args, ip=None, **kwargs):
@@ -16,3 +17,26 @@ def loss_factory(loss, Y, *args, **kwargs):
     if cls is None:
         raise ValueError("Loss with this name does not exist")
     return cls(Y=Y, *args, **kwargs)
+
+
+def generate_batches(X, Y, batch_size, shuffle=True):
+    num_samples = X.shape[-1]
+    num_batches = int(np.ceil(num_samples / batch_size))
+
+    if shuffle is True:
+        perm = np.random.permutation(num_samples)
+        X, Y = X[:, perm], Y[:, perm]
+
+    if num_batches == 1:
+        return X, Y
+
+    start = 0
+    for idx in range(num_batches):
+        end = (idx + 1) * batch_size
+
+        if end > num_samples:
+            yield X[:, start:], Y[:, start:], num_samples - start
+        else:
+            yield X[:, start:end], Y[:, start:end], batch_size
+
+        start = end

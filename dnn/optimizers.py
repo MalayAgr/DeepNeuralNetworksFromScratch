3913c59d8b242b18099b394da130a4cbf2adba9e
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-06-18 21:29:33 +0530

    Implement SGD with momentum

--- dnn/optimizers.py
+++ dnn/optimizers.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from .utils import loss_factory
+from .utils import loss_factory, generate_batches
 from dnn.layer import Layer
+import time
 
 
 class Optimizer(ABC):
-    def __init__(self, learning_rate, *args, loss="bse", **kwargs):
+    def __init__(self, learning_rate=0.01, *args, **kwargs):
         self.lr = learning_rate
-        self.loss = loss
 
     def get_layer_dA(self, dA_params):
         if isinstance(dA_params, Layer):
@@ -40,9 +40,75 @@ class Optimizer(ABC):
             dA = layer
 
     @abstractmethod
-    def update_params(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
     def optimize(self, model, X, Y, batch_size, epochs, shuffle=True):
         pass
+
+
+class SGD(Optimizer):
+    def __init__(self, learning_rate=0.01, *args, **kwargs):
+        momentum = kwargs.get("momentum", 0)
+
+        if not 0 <= momentum <= 1:
+            raise AttributeError("momentum should be between 0 and 1")
+
+        self.momentum = momentum
+        super().__init__(learning_rate=learning_rate, *args, **kwargs)
+
+    def init_velocities(self, model):
+        for layer in model.layers:
+            layer.velocities = {
+                "weights": np.zeros(shape=layer.weights.shape),
+                "biases": np.zeros(shape=layer.biases.shape),
+            }
+
+    def update_layer_velocities(self, layer):
+        dW = layer.gradients["weights"]
+        db = layer.gradients["biases"]
+
+        W_vel = layer.velocities["weights"]
+        b_vel = layer.velocities["biases"]
+
+        layer.velocities["weights"] = self.momentum * W_vel + (1 - self.momentum) * dW
+        layer.velocities["biases"] = self.momentum * b_vel + (1 - self.momentum) * db
+
+    def update_params_momentum(self, model):
+        for layer in model.layers:
+            self.update_layer_velocities(layer)
+            layer.weights -= self.lr * layer.velocities["weights"]
+            layer.biases -= self.lr * layer.velocities["biases"]
+
+    def update_params_no_momentum(self, model):
+        for layer in model.layers:
+            layer.weights -= self.lr * layer.gradients["weights"]
+            layer.biases -= self.lr * layer.gradients["biases"]
+
+    def get_update_function(self, model):
+        if self.momentum == 0:
+            return self.update_params_no_momentum
+
+        self.init_velocities(model)
+        return self.update_params_momentum
+
+    def optimize(self, model, X, Y, batch_size, epochs, loss="bse", shuffle=True):
+        cost, history = 0, []
+
+        update_func = self.get_update_function(model)
+
+        for epoch in range(epochs):
+            batches = generate_batches(X, Y, batch_size=batch_size, shuffle=shuffle)
+            for batch_X, batch_Y, size in batches:
+                self.train_size = size
+                # Forward pass
+                preds = model.predict(batch_X)
+                # Compute cost
+                loss_func = loss_factory(loss, batch_Y)
+                cost = loss_func.compute_loss(preds)
+                # Backprop
+                self.backprop(model, loss=loss_func, preds=preds)
+                # Update params
+                update_func(model)
+
+            print(f"Loss at the end of epoch {epoch + 1}: {cost: .9f}")
+            history.append(cost)
+
+        return history

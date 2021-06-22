d4af58b237233e99336ed247ccf7068a7881f875
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-06-22 19:03:55 +0530

    Add forward step for batch normalization

--- dnn/layer.py
+++ dnn/layer.py
@@ -4,17 +4,78 @@ from dnn.input_layer import Input
 from dnn.utils import activation_factory
 
 
+class BatchNorm:
+    def __init__(self, ip, epsilon=1e-7):
+        if not isinstance(ip, Layer):
+            raise AttributeError("ip should be an instance of Layer")
+
+        self.ip_layer = ip
+        self.epsilon = epsilon
+
+        self.gamma, self.beta = self.init_params()
+
+        self.std = None
+        self.Z_hat = None
+        self.norm = None
+
+    def init_params(self):
+        gamma = np.ones(shape=(self.ip_layer.units, 1))
+        beta = np.zeros(shape=(self.ip_layer.units, 1))
+        return gamma, beta
+
+    def compute_norm(self, X):
+        mean = np.mean(X, axis=1, keepdims=True)
+
+        var = np.var(X, axis=1, keepdims=True)
+        std = np.sqrt(var + self.epsilon)
+        self.std = std
+
+        Z_hat = (X - mean) / std
+        self.Z_hat = Z_hat
+
+        self.norm = self.gamma * Z_hat + self.beta
+        return self.norm
+
+    def get_layer_dZ(self, layer_dA):
+        activation_grads = self.ip_layer.activation.calculate_derivatives(self.norm)
+
+        d_norm = layer_dA * activation_grads
+
+        self.ip_layer.gradients = {
+            "gamma": np.sum(d_norm * self.std_ip, axis=1, keepdims=True),
+            "beta": np.sum(d_norm, axis=1, keepdims=True),
+        }
+
+        dZ_hat = d_norm * self.gamma
+
+        bs = self.ip_layer.get_ip().shape[-1]
+
+        dZ_hat_sum = np.sum(dZ_hat, axis=1, keepdims=True)
+
+        dZ = (
+            bs * dZ_hat
+            - dZ_hat_sum
+            - self.Z_hat * np.sum(dZ_hat, self.Z_hat, axis=1, keepdims=True)
+        )
+
+        return dZ / (bs * self.std)
+
+
 class Layer:
-    def __init__(self, ip, units, activation, initializer="he"):
+    def __init__(self, ip, units, activation, initializer="he", batch_norm=False):
         if not isinstance(ip, (Input, self.__class__)):
             msg = f"A {self.__class__.__name__} can have only instances of Input or itself as ip"
             raise AttributeError(msg)
 
         self.ip_layer = ip
+
         self.units = units
         self.activation = self.init_activation(activation)
+
         self.initializer = initializer
         self.weights, self.biases = self.init_params()
+
+        self.batch_norm = BatchNorm(self) if batch_norm is True else batch_norm
 
         self.linear = None
         self.activations = None
@@ -65,7 +126,12 @@ class Layer:
 
     def forward_step(self):
         linear = np.matmul(self.weights, self.get_ip()) + self.biases
-        activations = self.activation.calculate_activations(linear)
+
+        if self.batch_norm is not False:
+            norm = self.batch_norm.compute_norm(linear)
+            activations = self.activation.calculate_activations(norm)
+        else:
+            activations = self.activation.calculate_activations(linear)
 
         self.linear, self.activations = linear, activations
 

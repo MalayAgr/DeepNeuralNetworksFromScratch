a5956b3ef7428a4a9f908a3ed5e34ebd2916f908
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-07-02 00:37:07 +0530

    Add test for clipping in BCE

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
+
+from numpy.core.fromnumeric import size
 from dnn.loss import Loss, BinaryCrossEntropy, MeanSquaredError
-from dnn.activations import Sigmoid
+from dnn.activations import ReLU, Sigmoid
 
 import numpy as np
 
@@ -55,18 +57,46 @@ class LossTestCase:
                 self.assertIn(name, registry)
 
 
-class BSETestCase(LossTestCase, unittest.TestCase):
+class BCETestCase(LossTestCase, unittest.TestCase):
     loss_cls = BinaryCrossEntropy
 
     def loss_func(self):
         train_size = self.Y.shape[-1]
-        positive = self.Y * np.log(self.preds)
-        negative = (1 - self.Y) * np.log(1 - self.preds)
+
+        preds, epsilon = self.preds, BinaryCrossEntropy.epsilon
+
+        if 1.0 in preds:
+            preds = np.clip(preds, epsilon, 1.0 - epsilon)
+
+        positive = self.Y * np.log(preds)
+        negative = (1 - self.Y) * np.log(1 - preds)
         loss = np.sum(-(positive + negative)) / train_size
         return np.squeeze(loss)
 
     def loss_derivatives(self):
-        return (1 - self.Y) / (1 - self.preds) - self.Y / self.preds
+        preds, epsilon = self.preds, BinaryCrossEntropy.epsilon
+
+        if 1.0 in preds:
+            preds = np.clip(preds, epsilon, 1.0 - epsilon)
+
+        return (1 - self.Y) / (1 - preds) - self.Y / preds
+
+    def test_clipping(self):
+        Y = np.random.choice((0, 1), size=(1, 10))
+        preds = np.random.choice((0, 1), size=(1, 10))
+
+        self.Y = Y
+        self.preds = preds
+
+        expected_loss = self.loss_func()
+        loss = self.loss.compute_loss(Y, preds)
+
+        self.assertAlmostEqual(loss, expected_loss)
+
+        expected_grads = self.loss_derivatives()
+        grads = self.loss.compute_derivatives(Y, preds)
+
+        np.testing.assert_allclose(grads, expected_grads)
 
 
 class MSETestCase(LossTestCase, unittest.TestCase):

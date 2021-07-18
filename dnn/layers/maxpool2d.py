1d9286efe221240b3802797d08fe5c2cc00aa33e
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-07-18 13:11:18 +0530

    Make Conv2D and MaxPooling2D dynamic so that they can accept any input size

--- dnn/layers/maxpool2d.py
+++ dnn/layers/maxpool2d.py
@@ -33,14 +33,7 @@ class MaxPooling2D(BaseLayer):
 
         super().__init__(ip, trainable=False)
 
-        self.windows, self.ip_H, self.ip_W = self.input_shape()[:-1]
-
-        self.out_H = compute_conv_output_dim(
-            self.ip_H, self.pool_H, self.p_H, self.stride_H
-        )
-        self.out_W = compute_conv_output_dim(
-            self.ip_W, self.pool_W, self.p_W, self.stride_W
-        )
+        self.windows = self.input_shape()[0]
 
         self.pooled = None
 
@@ -54,11 +47,22 @@ class MaxPooling2D(BaseLayer):
     def output(self) -> np.ndarray:
         return self.pooled
 
+    def output_area(self) -> tuple[int, int]:
+        ip_shape = self.input_shape()
+        ipH, ipW = ip_shape[1], ip_shape[2]
+
+        oH = compute_conv_output_dim(ipH, self.pool_H, self.p_H, self.stride_H)
+        oW = compute_conv_output_dim(ipW, self.pool_W, self.p_W, self.stride_W)
+
+        return oH, oW
+
     def output_shape(self) -> tuple:
         if self.pooled is not None:
             return self.pooled.shape
 
-        return self.windows, self.out_H, self.out_W, None
+        oH, oW = self.output_area()
+
+        return self.windows, oH, oW, None
 
     def _get_pool_outputs(self, ip: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         ip_shape = ip.shape
@@ -78,7 +82,7 @@ class MaxPooling2D(BaseLayer):
         maximums = maximums.reshape(*ip_shape[:-1])
         mask = mask.reshape(*ip_shape)
 
-        shape = (self.windows, self.out_H, self.out_W, -1)
+        shape = (self.windows, *self.output_area(), -1)
 
         return np.swapaxes(maximums, 0, -1).reshape(*shape), mask
 
@@ -89,7 +93,7 @@ class MaxPooling2D(BaseLayer):
             X=X,
             kernel_size=self.pool_size,
             stride=self.stride,
-            output_size=(self.out_H, self.out_W),
+            output_size=self.output_area(),
             reshape=(self.windows, self.pool_H * self.pool_W, X.shape[-1]),
         )
 
@@ -112,7 +116,7 @@ class MaxPooling2D(BaseLayer):
 
         dX = accumulate_dX_conv(
             dX_shape=(dA.shape[0], self.windows, *self._padded_shape),
-            output_size=(self.out_H, self.out_W),
+            output_size=self.output_area(),
             dIp=self._dX_share * dA[..., None],
             stride=self.stride,
             kernel_size=self.pool_size,

5851c5cca1937181081cd34ebbb6c65592e6a496
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-07-19 04:04:09 +0530

    Refactor convolution computation and implement depthwise conv2d

--- dnn/layers/utils.py
+++ dnn/layers/utils.py
@@ -73,9 +73,39 @@ def vectorize_for_conv(
     return vectorized_ip
 
 
-def vectorize_kernel_for_conv(kernel) -> np.ndarray:
-    filters = kernel.shape[-1]
-    return kernel.reshape(-1, filters)
+def vectorize_kernel_for_conv(kernel, reshape=None) -> np.ndarray:
+    if reshape is None:
+        filters = kernel.shape[-1]
+        reshape = (-1, filters)
+    return kernel.reshape(*reshape)
+
+
+def _prepare_ip_for_conv(
+    X: np.ndarray,
+    kernel_size: Tuple[int, int],
+    stride: Tuple[int, int] = (1, 1),
+    padding: Tuple[int, int] = (0, 0),
+    vec_reshape: Optional[Tuple] = None,
+) -> Tuple[np.ndarray, int, int]:
+
+    ipH, ipW = X.shape[1:-1]
+    kH, kW = kernel_size
+    sH, sW = stride
+    pH, pW = padding
+
+    oH = compute_conv_output_dim(ipH, kH, pH, sH)
+    oW = compute_conv_output_dim(ipW, kW, pW, sW)
+
+    if padding != (0, 0):
+        X, _ = pad(X, pH, pW)
+
+    X = vectorize_for_conv(X, (kH, kW), stride, (oH, oW), reshape=vec_reshape)
+
+    return X, oH, oW
+
+
+def convolve(X, weights):
+    return np.matmul(X, weights[None, ...], dtype=np.float32)
 
 
 def convolve2d(
@@ -90,29 +120,62 @@ def convolve2d(
     Tuple[np.ndarray, np.ndarray],
     Tuple[np.ndarray],
 ]:
-
-    ipH, ipW = X.shape[1:-1]
     kH, kW, filters = kernel.shape[1:]
-    sH, sW = stride
-    pH, pW = padding
 
-    oH = compute_conv_output_dim(ipH, kH, pH, sH)
-    oW = compute_conv_output_dim(ipW, kW, pW, sW)
-
-    if padding != (0, 0):
-        X, _ = pad(X, pH, pW)
-
-    X = vectorize_for_conv(X, (kH, kW), stride, (oH, oW))
+    X, oH, oW = _prepare_ip_for_conv(
+        X=X, kernel_size=(kH, kW), stride=stride, padding=padding
+    )
 
     X = np.moveaxis(X, -1, 0)
 
     weights = vectorize_kernel_for_conv(kernel)
 
-    convolution = np.matmul(X, weights[None, ...], dtype=np.float32)
+    convolution = convolve(X, weights)
 
     shape = (filters, oH, oW, -1)
 
     ret_val = (np.swapaxes(convolution, 0, 2).reshape(shape),)
+
+    if return_vec_ip is True:
+        ret_val += (X,)
+
+    if return_vec_kernel is True:
+        ret_val += (weights,)
+
+    return ret_val
+
+
+def depthwise_convolve2d(
+    X: np.ndarray,
+    kernel: np.ndarray,
+    stride: Tuple[int, int] = (1, 1),
+    padding: Tuple[int, int] = (0, 0),
+    return_vec_ip: bool = False,
+    return_vec_kernel: bool = False,
+) -> Union[
+    Tuple[np.ndarray, np.ndarray, np.ndarray],
+    Tuple[np.ndarray, np.ndarray],
+    Tuple[np.ndarray],
+]:
+    ipC, kH, kW, multiplier = kernel.shape
+
+    X, oH, oW = _prepare_ip_for_conv(
+        X=X,
+        kernel_size=(kH, kW),
+        stride=stride,
+        padding=padding,
+        vec_reshape=(ipC, (kH * kW), -1),
+    )
+
+    X = X.transpose(-1, 1, 0, 2)
+
+    weights = vectorize_kernel_for_conv(kernel, reshape=(ipC, -1, multiplier))
+
+    convolution = convolve(X, weights)
+
+    shape = (ipC * multiplier, oH, oW, -1)
+
+    ret_val = (np.swapaxes(convolution, 0, -1).reshape(shape),)
 
     if return_vec_ip is True:
         ret_val += (X,)

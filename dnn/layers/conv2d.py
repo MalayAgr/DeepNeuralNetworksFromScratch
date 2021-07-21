Fix loss gradients by scaling by batch size

dnn/layers/conv2d.py
@@ -26,7 +26,7 @@ class Conv2D(Conv):
 
     def _param_scale(self) -> int:
         oH, oW = self.output_area()
-        return self._vec_ip.shape[0] * oH * oW
+        return oH * oW
 
     def _reshape_dZ(self, dZ: np.ndarray) -> np.ndarray:
         return np.swapaxes(dZ, 0, -1).reshape(dZ.shape[-1], -1, self.filters)

dnn/layers/dense.py
@@ -81,21 +81,21 @@ class Dense(BaseLayer):
 
     def backprop_step(self, dA: np.ndarray, *args, **kwargs) -> np.ndarray:
         ip = self.input()
-        m = ip.shape[-1]
 
         dA = self.activation.backprop_step(dA, ip=self.linear)
 
         reg_param = kwargs.pop("reg_param", 0.0)
 
-        dW = np.matmul(dA, ip.T, dtype=np.float32) / m
+        dW = np.matmul(dA, ip.T, dtype=np.float32)
 
         if reg_param > 0:
+            m = ip.shape[-1]
             dW += (reg_param / m) * self.weights
 
         self.gradients["weights"] = dW
 
         if self.use_bias:
-            self.gradients["biases"] = np.sum(dA, keepdims=True, axis=1) / m
+            self.gradients["biases"] = np.sum(dA, keepdims=True, axis=1)
 
         if self.requires_dX is False:
             self.reset_attrs()

dnn/loss.py
@@ -90,6 +90,7 @@ class BinaryCrossEntropy(Loss):
 
         grad = (1 - labels) / (1 - preds)
         grad -= labels / preds
+        grad /= labels.shape[-1]
         return grad
 
 
@@ -104,7 +105,7 @@ class MeanSquaredError(Loss):
         return np.squeeze(loss)
 
     def loss_derivative(self, labels: np.ndarray, preds: np.ndarray) -> np.ndarray:
-        return preds - labels
+        return (preds - labels) / labels.shape[-1]
 
 
 class CategoricalCrossEntropy(Loss):
@@ -118,4 +119,5 @@ class CategoricalCrossEntropy(Loss):
     def loss_derivative(self, labels: np.ndarray, preds: np.ndarray) -> np.ndarray:
         grad = -labels
         grad /= preds
+        grad /= labels.shape[-1]
         return grad
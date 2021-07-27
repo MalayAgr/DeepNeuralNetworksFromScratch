3aa0086fe8be2f4f904057b138190712c467ec8b
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-07-27 14:25:42 +0530

    Reimplement Model to support multiple inputs and outputs

--- dnn/training/model.py
+++ dnn/training/model.py
@@ -1,27 +1,41 @@
 from __future__ import annotations
-from dnn.training.graph.nodes import LayerNode
 
-from typing import List, Tuple
+from typing import Any, List, Tuple, Union
 
 import numpy as np
 from dnn import Input
 from dnn.layers.base_layer import BaseLayer
-from dnn.optimizers import Optimizer
 from dnn.training.graph.core import ComputationGraph
 
-from .model_utils import build_graph_for_model
+from .model_utils import build_graph_for_model, flatten_layers
 
 
 class Model:
     def __init__(
-        self, ip_layer: Input, op_layer: BaseLayer, graph: ComputationGraph = None
+        self,
+        inputs: Union[List[Input], Input],
+        outputs: Union[List[BaseLayer], BaseLayer],
+        *args,
+        name: str = None,
+        graph: ComputationGraph = None,
+        **kwargs,
     ) -> None:
-        self.ip_layer = ip_layer
-        self.op_layer = op_layer
+        if not isinstance(inputs, List):
+            inputs = [inputs]
 
-        self._built = False
+        if not isinstance(outputs, List):
+            outputs = [outputs]
+
+        self.inputs = inputs
+        self.outputs = outputs
+
+        layers: List[BaseLayer] = []
+        flatten_layers(inputs=inputs, outputs=outputs, accumulator=layers)
+        self.layers = layers
 
         self._graph = graph
+
+        self._built = False
 
         self.opt = None
         self.loss_func = None
@@ -31,106 +45,62 @@ class Model:
         return self._built
 
     @built.setter
-    def built(self, value: bool) -> None:
+    def built(self, value: bool):
         self._built = value
 
-    def build(self):
+    def build(self) -> Any:
         self._graph = build_graph_for_model(
-            self.ip_layer, self.op_layer, graph=self._graph
+            layers=self.layers,
+            inputs=self.inputs,
+            outputs=self.outputs,
+            graph=self._graph,
         )
         self.built = True
 
-    def predict(self, X: np.ndarray):
+    def fetch_layer(self, name: str = None, idx: int = None) -> BaseLayer:
+        num_layers = len(self.layers)
+
+        if name is not None and idx is not None:
+            raise ValueError("Specify only one of name or idx at a time.")
+
+        if idx is not None:
+            if num_layers <= idx:
+                raise ValueError(
+                    f"{idx} is out of bounds since the model "
+                    f"has only {num_layers} layers."
+                )
+
+            return self.layers[idx]
+
+        if name is not None:
+            for layer in self.layers:
+                if layer.name == name:
+                    return layer
+            raise ValueError(f"No layer with name {name} exists in the model.")
+
+        raise ValueError("Specify either a name or an index to fetch a layer.")
+
+    def predict(
+        self, inputs: Union[np.ndarray, List[np.ndarray]]
+    ) -> Union[np.ndarray, Tuple[np.ndarray]]:
+
         if not self.built:
             self.build()
 
-        self.ip_layer.ip = X
+        if not isinstance(inputs, List):
+            inputs = [inputs]
+
+        if len(inputs) != len(self.inputs):
+            raise ValueError(
+                "Unexpected number of inputs passed to the model. "
+                f"It expected {len(self.inputs)} but got {len(inputs)}."
+            )
+
+        for ip, X in zip(self.inputs, inputs):
+            ip.ip = X
+
         op = self._graph.forward_propagation()
 
+        if len(self.outputs) == 1:
+            op = op[0]
         return op
-
-
-# class Model:
-#     def __init__(self, ip_layer: LayerInput, op_layer: LayerInput):
-#         self.ip_layer = ip_layer
-#         self.op_layer = op_layer
-
-#         self.layers, self.trainable_layers = self._deconstruct(ip_layer, op_layer)
-
-#         self.opt = None
-
-#     @staticmethod
-#     def _deconstruct(
-#         ip_layer: LayerInput, op_layer: LayerInput
-#     ) -> Tuple[List[BaseLayer], List[BaseLayer]]:
-#         layers, trainable_layers = [], []
-
-#         layer = op_layer
-
-#         while layer is not ip_layer:
-#             layers.append(layer)
-
-#             if layer.trainable is True:
-#                 trainable_layers.append(layer)
-
-#             layer = layer.ip_layer
-
-#         layer.requires_dX = False
-
-#         return layers[::-1], trainable_layers[::-1]
-
-#     def __str__(self) -> str:
-#         layers = ", ".join([str(l) for l in self.layers])
-#         return f"{self.__class__.__name__}({self.ip_layer}, {layers})"
-
-#     def __repr__(self) -> str:
-#         return self.__str__()
-
-#     def predict(self, X: np.ndarray) -> np.ndarray:
-#         self.ip_layer.ip = X
-
-#         for layer in self.layers:
-#             layer.forward_step()
-
-#         return self.op_layer.output()
-
-#     def compile(self, opt: Optimizer):
-#         if not isinstance(opt, Optimizer):
-#             raise ValueError("opt should be an instance of a subclass of Optimizer.")
-
-#         for layer in self.layers:
-#             layer.build()
-
-#         self.opt = opt
-
-#     def set_operation_mode(self, training: bool) -> None:
-#         for layer in self.layers:
-#             layer.is_training = training
-
-#     def train(
-#         self,
-#         X: np.ndarray,
-#         Y: np.ndarray,
-#         batch_size: int,
-#         epochs: int,
-#         *args,
-#         loss: str = "bce",
-#         shuffle: bool = True,
-#         **kwargs,
-#     ) -> list:
-#         if X.shape[-1] != Y.shape[-1]:
-#             raise ValueError("X and Y should have the same number of samples.")
-
-#         if Y.shape[0] != self.layers[-1].units:
-#             msg = "Y should have the same number of rows as number of units in the final layer."
-#             raise ValueError(msg)
-
-#         self.set_operation_mode(training=True)
-
-#         history = self.opt.optimize(
-#             self, X, Y, batch_size, epochs, *args, loss=loss, shuffle=shuffle, **kwargs
-#         )
-
-#         self.set_operation_mode(training=False)
-
-#         return history

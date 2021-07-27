0064a84a4d35109bf4dc56d1bae5114bc7b7fa83
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-07-28 00:02:44 +0530

    Expose API

--- dnn/__init__.py
+++ dnn/__init__.py
@@ -1,2 +1,5 @@
 from .input_layer import Input
+from .training import optimizers
 from .training.graph import context as ctx
+from .training.model import Model
+from .training import model_utils
---
+++ dnn/training/optimizers/__init__.py
@@ -0,0 +1,4 @@
+from .base_optimizer import Optimizer
+from .adam import Adam
+from .sgd import SGD
+from .rmsprop import RMSProp

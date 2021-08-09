8dc67fa7cf558d78647cf57873bee47b91cea6ca
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-08-09 13:15:42 +0530

    Add validation

--- dnn/layers/add.py
+++ dnn/layers/add.py
@@ -10,13 +10,24 @@ class Add(MultiInputBaseLayer):
 
         self.add = None
 
+    def _validate_same_shape(self):
+        shapes = self.input_shape()
+        first_shape = shapes[0]
+        if any(shape != first_shape for shape in shapes[1:]):
+            msg = (
+                "All inputs must output the same shape. "
+                f"Expected shape: {first_shape}"
+            )
+            raise ValueError(msg)
+
     def output(self) -> np.ndarray:
         return self.add
 
     def output_shape(self) -> Tuple:
-        return self.input_shape()
+        return self.input_shape()[0]
 
     def forward_step(self, *args, **kwargs) -> np.ndarray:
+        self._validate_same_shape()
         self.add = np.add.reduce(self.input())
         return self.add
 

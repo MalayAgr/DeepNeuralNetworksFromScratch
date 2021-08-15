3c01f8c6be230b00e992b8aedc3c5589388dc7ad
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-08-15 15:02:05 +0530

    Expose SeparableConv2D

--- dnn/layers/__init__.py
+++ dnn/layers/__init__.py
@@ -1,11 +1,12 @@
+from dnn.layers.add import Add
 from dnn.layers.avgpool2d import AveragePooling2D
 from dnn.layers.base_layer import BaseLayer, LayerInput
 from dnn.layers.batch_norm import BatchNorm
+from dnn.layers.concatenate import Concatenate
 from dnn.layers.conv2d import Conv2D
 from dnn.layers.dense import Dense
+from dnn.layers.depthwise_conv2d import DepthwiseConv2D
 from dnn.layers.dropout import Dropout
 from dnn.layers.flatten import Flatten
 from dnn.layers.maxpool2d import MaxPooling2D
-from dnn.layers.depthwise_conv2d import DepthwiseConv2D
-from dnn.layers.add import Add
-from dnn.layers.concatenate import Concatenate
+from dnn.layers.separable_conv2d import SeparableConv2D

e3525024dc98d8f6750a3e7507a4671c0d810ff1
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-09-24 18:03:13 +0530

    Add __future__ import and sort

--- dnn/layers/utils/generic_utils.py
+++ dnn/layers/utils/generic_utils.py
@@ -1,6 +1,9 @@
-from dnn.layers.base_layer import LayerInput
+from __future__ import annotations
+
 from typing import Optional, Union
+
 from dnn.layers.activations import Activation
+from dnn.layers.base_layer import LayerInput
 
 
 def activation_factory(
@@ -9,7 +12,7 @@ def activation_factory(
     registry = Activation.get_activation_classes()
     cls = registry.get(activation)
     if cls is None:
-        raise ValueError("Activation with this name does not exist")
+        raise ValueError("Activation with this name does not exist.")
     return cls(ip=ip, *args, **kwargs)
 
 

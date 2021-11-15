636645fc46c9e804bd47f3272e9f261c4342dc60
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-11-16 00:12:51 +0530

    Remove unnecessary utils

--- dnn/utils.py
+++ dnn/utils.py
@@ -11,6 +11,7 @@ from dnn.loss import Loss
 def optional_jit(
     _func: Callable = None, *, nopython: bool = True, forceobj: bool = False
 ) -> Callable:
+    @functools.wraps(_func)
     def decorator(_func: Callable):
         try:
             from numba import jit
@@ -28,6 +29,7 @@ def loss_factory(loss: str) -> Loss:
     if cls is None:
         raise ValueError("Loss with this name does not exist")
     return cls()
+
 
 @optional_jit
 def generate_batches(
@@ -58,23 +60,3 @@ def generate_batches(
     if num_samples % batch_size != 0:
         start = batch_size * num_full_batches
         yield X[..., start:], Y[..., start:], num_samples - start
-
-
-def compute_l2_cost(model: "Model", reg_param: float, cost: float) -> float:
-    norm = np.add.reduce([np.linalg.norm(layer.weights) ** 2 for layer in model.layers])
-
-    m = model.ip_layer.ip.shape[-1]
-
-    return cost + (reg_param * norm) / (2 * m)
-
-
-def rgetattr(obj, attr, *args):
-    def _getattr(obj, attr):
-        return getattr(obj, attr, *args)
-
-    return functools.reduce(_getattr, [obj] + attr.split("."))
-
-
-def rsetattr(obj, attr, val):
-    pre, _, post = attr.rpartition(".")
-    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
